b525a0fe258199c04d2da981b2088262289bc466
PedroCruz <contatocreed67@gmail.com>
2023-11-14 10:16:35 -0400

    rota de listagem de usuarios

---
+++ src/controllers/listUsersController.js
@@ -0,0 +1,7 @@
+import { listUserServices } from "../services/listUsersServices.js";
+
+export const listUserController = (request, response) => {
+  const [status, data] = listUserServices();
+
+  return response.status(status).json(data);
+};
---
+++ src/services/listUsersServices.js
@@ -0,0 +1,5 @@
+import users from "../database.js";
+
+export const listUserServices = () => {
+  return [200, users];
+};
