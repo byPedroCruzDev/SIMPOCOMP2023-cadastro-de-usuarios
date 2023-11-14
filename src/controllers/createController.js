1348fcd45ee19e8986bb3ed0e84ee5e4551343bd
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-11-14 18:56:11 -0400

    style: formatted files

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -1,13 +1,13 @@
-import { createUserServices } from "../services/createUserServices.js";
+import { createUserServices } from '../services/createUserServices.js'
 
 const createUserController = async (request, response) => {
-  const [status, data] = await createUserServices(request.body);
-  return response.status(status).json(data);
-};
+  const [status, data] = await createUserServices(request.body)
+  return response.status(status).json(data)
+}
 
-export default createUserController;
+export default createUserController
 
-//import, export, request, response
-//[]
-//request.body, json
+// import, export, request, response
+// []
+// request.body, json
 //
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
-//esse é nosso "banco de dados" improvisado
-//os arquivos que manipularmos serao salvos aqui
-//mas ficarao salvos apens durante a execusao do programa
-const users = [];
+// esse é nosso "banco de dados" improvisado
+// os arquivos que manipularmos serao salvos aqui
+// mas ficarao salvos apens durante a execusao do programa
+const users = []
 
-export default users;
+export default users
