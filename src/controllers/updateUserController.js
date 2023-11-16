b7ef83fdcb94d0693377940f777167298d826588
PedroCruz <contatocreed67@gmail.com>
2023-11-16 09:04:19 -0400

    adicionando a rota de atualizacao de um usuario

---
+++ src/controllers/updateUserController.js
@@ -0,0 +1,9 @@
+import { updateUserServices } from "../services/updateUserServices.js";
+
+export const updateUserController = async (request, response) => {
+  const [status, data] = await updateUserServices(request.id, request.body);
+
+  //o password é deletado no retorno por boas praticas
+  delete data.password;
+  return response.status(status).json(data);
+};
