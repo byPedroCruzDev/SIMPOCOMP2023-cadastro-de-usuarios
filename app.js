7ae9b43617732185b5b0448a6a2299d3a5d0eac8
PedroCruz <contatocreed67@gmail.com>
2023-11-17 10:14:07 -0400

    fix:PORT

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 //Cria um instancia que facilita utilizacao futura
 const app = express();
 //define a porta em que o nosso servidor ira rodar.
-const port = 3000;
+const PORT = 3000;
 
 //define uma rota, onde podemos fazer uma requisicao do tipo get
 app.get("/", (request, response) => {
@@ -14,8 +14,8 @@ app.get("/", (request, response) => {
 //a funcao listen "ouve" tudo que acontece em um determinado local/porta
 //ele recebe dois parametros, onde ele ira ouvir, e uma funcao que indica o que o servidore
 //ira ouvir.
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 //local onde voce pode abrir no seu navegador.
