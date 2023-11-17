d3c2e0362d67446bce5b4b83b93a86ad4f9078c9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-11-17 17:18:39 -0400

    lint: lint code

--- src/controllers/listUserByIdController.js
+++ src/controllers/listUserByIdController.js
@@ -1,7 +1,7 @@
-import { listUserByIdServices } from "../services/listUserByIdServices.js";
+import { listUserByIdServices } from '../services/listUserByIdServices.js'
 
 export const listUserByIdController = (request, response) => {
-  const [status, data] = listUserByIdServices(request.params.id);
+  const [status, data] = listUserByIdServices(request.params.id)
 
-  return response.status(status).json(data);
-};
+  return response.status(status).json(data)
+}
