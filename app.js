11507d36fac072b661c80a10fbe8cfe0c3d01c13
PedroCruz <contatocreed67@gmail.com>
2023-11-19 14:38:48 -0400

    fix: added single quote

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-//Importando a o framework express para ser utilizado nesse arquivo
-import express from "express";
+// Importando a o framework express para ser utilizado nesse arquivo
+import express from 'express'
 
-//Cria um instancia que facilita utilizacao futura
-const app = express();
-//define a porta em que o nosso servidor ira rodar.
-const PORT = 3000;
+// Cria um instancia que facilita utilizacao futura
+const app = express()
+// define a porta em que o nosso servidor ira rodar.
+const PORT = 3000
 
-//define uma rota, onde podemos fazer uma requisicao do tipo get
-app.get("/", (request, response) => {
-  response.send("Hello world");
+// define uma rota, onde podemos fazer uma requisicao do tipo get
+app.get('/', (request, response) => {
+  response.send("Hello world")
 });
 
-//a funcao listen "ouve" tudo que acontece em um determinado local/porta
-//ele recebe dois parametros, onde ele ira ouvir, e uma funcao que indica o que o servidore
-//ira ouvir.
+// a funcao listen "ouve" tudo que acontece em um determinado local/porta
+// ele recebe dois parametros, onde ele ira ouvir, e uma funcao que indica o que o servidore
+// ira ouvir.
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`)
 });
 
 //local onde voce pode abrir no seu navegador.
