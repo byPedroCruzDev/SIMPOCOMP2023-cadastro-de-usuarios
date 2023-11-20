ea82d0616674ddd69742522af1a1b6a0318d09e3
PedroCruz <contatocreed67@gmail.com>
2023-11-20 00:11:47 -0400

    added delete in password return

--- src/controllers/listUserByIdController.js
+++ src/controllers/listUserByIdController.js
@@ -3,5 +3,7 @@ import { listUserByIdServices } from '../services/listUserByIdServices.js'
 export const listUserByIdController = (request, response) => {
   const [status, data] = listUserByIdServices(request.params.id)
 
+  delete data.password
+
   return response.status(status).json(data)
 }
--- src/services/listUsersServices.js
+++ src/services/listUsersServices.js
@@ -2,7 +2,8 @@ import users from '../database.js'
 
 export const listUserServices = () => {
   try {
-    return [200, users]
+    const respUser = users.filter((users) => delete users.password)
+    return [200, respUser]
   } catch (error) {
     return [404, { error: 'User not found' }]
   }
